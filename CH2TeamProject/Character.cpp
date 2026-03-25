aba68382a0a48d2e3dc7bf095fff4179228faf9b
Kindle <kindlekim2377@gmail.com>
2026-03-25 21:48:37 +0900

    2-a-1. 본인 파일 수정(Mine 폴더)
    - Character 코드를 추가하였습니다.

--- CH2TeamProject/Character.cpp
+++ CH2TeamProject/Character.cpp
@@ -1,1 +1,22 @@
 #include "Character.h"
+#include <iostream>
+
+using namespace std;
+
+Character::Character(string nickname) : nickname(nickname), level(1), HP(100), MP(20),
+power(20), defence(20), accuracy(20), speed(20) {
+}
+
+void Character::printPlayerStatus() {
+    cout << "------------------------------------" << endl;
+    cout << "* 현재 능력치" << endl;
+    cout << "닉네임: " << nickname << endl;
+    cout << "Lv. " << level << endl;
+    cout << "HP: " << HP << endl;
+    cout << "MP: " << MP << endl;
+    cout << "공격력: " << power << endl;
+    cout << "방어력: " << defence << endl;
+    cout << "정확도: " << accuracy << endl;
+    cout << "속도: " << speed << endl;
+    cout << "------------------------------------" << endl;
+}
